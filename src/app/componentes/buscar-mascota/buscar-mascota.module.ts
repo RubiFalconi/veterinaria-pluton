6214c70d6424d9f9e35ebd370a300c06fa1ec58a
ChugnasIvan <ivan@ad-comp.net>
2023-02-06 22:19:07 -0500

    @ChugnasIvan - buscarcar mascota

---
+++ src/app/componentes/buscar-mascota/buscar-mascota.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { BuscarMascotaRoutingModule } from './buscar-mascota-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BuscarMascotaRoutingModule
+  ]
+})
+export class BuscarMascotaModule { }
