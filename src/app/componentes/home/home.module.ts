0d8df72b2ed961ece5d08e75e623f620b5e15683
ChugnasIvan <ivan@ad-comp.net>
2023-02-07 01:48:14 -0500

    @ChugnasIvan - add login

--- src/app/componentes/home/home.module.ts
+++ src/app/componentes/home/home.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from 'src/app/componentes/home/home-routing.module';
 import { HomeComponent } from './home.component';
 
-import {MatButtonModule} from '@angular/material/button';
 import { BuscarMascotaComponent } from '../buscar-mascota/buscar-mascota.component';
 import { LoginComponent } from '../login/login.component';
 
+// MATERIAL
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,6 +22,10 @@ import { LoginComponent } from '../login/login.component';
     CommonModule,
     HomeRoutingModule,
     MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class HomeModule { }
