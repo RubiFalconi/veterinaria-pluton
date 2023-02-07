25c2024f86d84083fe458d9671ed1755f40e68e9
RubiFalconi <rubifm.19@gmail.com>
2023-02-07 01:14:24 -0500

    avance maqueta buscar-mascota

--- src/app/componentes/home/home.module.ts
+++ src/app/componentes/home/home.module.ts
@@ -6,18 +6,34 @@ import { HomeComponent } from './home.component';
 import {MatButtonModule} from '@angular/material/button';
 import { BuscarMascotaComponent } from '../buscar-mascota/buscar-mascota.component';
 import { LoginComponent } from '../login/login.component';
-
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSelectModule } from '@angular/material/select';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     HomeComponent,
     BuscarMascotaComponent,
-    LoginComponent
+    LoginComponent,
+    
+    
   ],
   imports: [
     CommonModule,
     HomeRoutingModule,
     MatButtonModule,
+    FormsModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSelectModule,
+    MatIconModule
   ]
 })
 export class HomeModule { }
